af839401eae163c93e9870b1059e44f32ef601bd
TH_Toufique <138722742+TH-Toufique@users.noreply.github.com>
2024-09-26 14:40:41 +0600

    Initial Commit
    added sign up and sign in page

---
+++ app/src/main/java/com/ripper/communicator/activity/SignInActivity.java
@@ -0,0 +1,38 @@
+package com.ripper.communicator.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ripper.communicator.R;
+import com.ripper.communicator.databinding.ActivitySignInBinding;
+
+
+public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        setListener();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    private void setListener()
+    {
+        binding.newAccountTv.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+    }
+}
---
+++ app/src/main/java/com/ripper/communicator/activity/SignUpActivity.java
@@ -0,0 +1,37 @@
+package com.ripper.communicator.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ripper.communicator.R;
+import com.ripper.communicator.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        setListener();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void setListener()
+    {
+        binding.signInTv.setOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
+    }
+
+}
